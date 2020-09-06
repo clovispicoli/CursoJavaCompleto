35d745057e6cb18440f8ecb5c13442f6e77e277b
Clovis Picoli <clovispicoli@gmail.com>
2020-09-06 20:54:41 -0300

    Atualizacao

--- src/course/Program.java
+++ src/course/Program.java
@@ -5,29 +5,40 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Entre com os tres numero: ");
-		
+
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-		
-		if (a > b && a > c) {
-			System.out.println("Superior: " + a);
 
-		}
-		else if (b > c) {
-			System.out.println("Superior: " + b);
-			
-		}
-		else {
-			System.out.println("Superior: " + c);
-			
-		}
+		int superior = max(a, b, c);
+
+		showResult(superior);
 
 		sc.close();
+	}
+
+
+	public static int max(int x, int y, int z) {
+
+		int aux;
+		if (x > y && x > z) {
+			aux = x;
+
+		}else if (y > z) {
+			aux = y;
+
+		}else {
+			aux = z;
+		}
+		return aux;
+	}
+	public static void showResult(int value) {
+		System.out.println("Supeior: " + value);
+
 	}
 
 }
