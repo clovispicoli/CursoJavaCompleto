31a2840c5a0bd3ce860aa37217c0c6d81448a3c9
Clovis Picoli <clovispicoli@gmail.com>
2020-09-05 21:55:59 -0300

    Resolvido exercicio 3

---
+++ src/course/exercicio_03.java
@@ -0,0 +1,29 @@
+package course;
+
+import java.util.Scanner;
+
+public class exercicio_03 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int dif;
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int D = sc.nextInt();
+		
+		dif = A * B - C * D;
+		
+		
+		System.out.println("Diferenca: " + dif);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
